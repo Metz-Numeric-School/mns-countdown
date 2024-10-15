67682eebdf7e275ad8277e597eb5fede74fd9995
GamorySensei <theo.gamory@metznumericschool.fr>
2024-10-15 17:32:43 +0200

    Màj

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,7 +6,7 @@ const minutesElement = countdownElement.querySelector('#minutes');
 const secondsElement = countdownElement.querySelector('#seconds');
 
 // EVENT DATE
-const endDate = new Date('2024-12-11 08:30:00');
+const endDate = new Date('2024-10-15 18:30:00');
 let secondsLeft = (endDate.getTime() - new Date().getTime()) / 1000;
 
 function updateCountdown() {
